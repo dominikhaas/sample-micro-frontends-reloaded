662cebac2503733a3a1a308d4a803c599e9f2c66
Dominik <dominik.hg.haas@googlemail.com>
2024-10-25 15:28:06 +0200

    Fix warning for custom web components

--- qa-locations-shell/vite.config.ts
+++ qa-locations-shell/vite.config.ts
@@ -6,7 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat tags with prefix profile- as custom elements
+          isCustomElement: (tag) => tag.includes('profile-') || tag.includes('location-')
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
