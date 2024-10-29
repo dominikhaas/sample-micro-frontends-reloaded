be35d53a61b8b4419a145a71d1ec5a5f86480535
Dominik <dominik.hg.haas@googlemail.com>
2024-10-29 14:05:15 +0100

    Created sample logging library

---
+++ logging-lib/dist/my-library.es.js
@@ -0,0 +1,6 @@
+function l(o, n) {
+  console.log(`${o} - ${n}`);
+}
+export {
+  l as logWarn
+};
---
+++ logging-lib/src/main.ts
@@ -0,0 +1,6 @@
+
+
+
+export function logWarn(frontendId: string, message: string): void {
+    console.log(`${frontendId} - ${message}`);
+}
